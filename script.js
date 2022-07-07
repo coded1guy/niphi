c9d37eab7c442e9502f8aaccc1e8ee74822334ed
coded1guy <grggmmnl@gmail.com>
2022-07-07 21:59:06 +0100

    update

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ console.log("navbar debug");
 
 
 // CAROUSEL
+document.getElementById("slide1").addEventListener("animationend", () => {
+    console.log("o ti pari")
+    document.getElementById("slide1").style.animation = "disappear 1s ease-out";
+    document.querySelector("h1").style.animationIterationCount = "1";
+})
