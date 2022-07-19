7850d373b0246e64b95055134e14fdd4a423f9a1
coded1guy <grggmmnl@gmail.com>
2022-07-19 12:46:50 +0100

    navbar looks good

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // NAVBAR
+// control for the toggling the dropdown and the menu icon
 console.log("navbar debug");
 let openMenuBtn = document.querySelector("button#menu");
 let closeMenuBtn = document.querySelector("button#close");
@@ -17,10 +18,3 @@ closeMenuBtn.onclick = () => {
     dd.classList.add("remove-hover");
     ddList.classList.add("remove-hover");
 }
-
-// CAROUSEL
-document.getElementById("slide1").addEventListener("animationend", () => {
-    console.log("o ti pari")
-    document.getElementById("slide1").style.animation = "disappear 1s ease-out";
-    document.querySelector("h1").style.animationIterationCount = "1";
-})
