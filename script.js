d94c1425747629d7026652066c0788815edcb4a3
Emmanuel Olubiyi <42422184+coded1guy@users.noreply.github.com>
2022-07-19 10:06:04 +0100

    pushing dirty codes, please forgive me

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 // NAVBAR
 console.log("navbar debug");
+let openMenuBtn = document.querySelector("button#menu");
+let closeMenuBtn = document.querySelector("button#close");
+let dd = document.querySelector("header > nav > div");
+let ddList = document.querySelector("header > nav > div > ul");
 
+openMenuBtn.onclick = () => {
+    openMenuBtn.style.display = "none";
+    closeMenuBtn.style.display = "block";
+    dd.classList.remove("remove-hover");
+    ddList.classList.remove("remove-hover");
+}
+closeMenuBtn.onclick = () => {
+    openMenuBtn.style.display = "block";
+    closeMenuBtn.style.display = "none";
+    dd.classList.add("remove-hover");
+    ddList.classList.add("remove-hover");
+}
 
 // CAROUSEL
 document.getElementById("slide1").addEventListener("animationend", () => {
